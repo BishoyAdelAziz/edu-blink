afe48f8b436f13fca7cbafb7624fd3dd0815ec9f
Bishoy Adel <105159347+BishoyAdelAziz@users.noreply.github.com>
2026-07-11 03:05:45 +0300

    fix: fixed the elements are not in a sequentially-descending order by using h5 instead of h4

--- components/features/Course/course-topics.tsx
+++ components/features/Course/course-topics.tsx
@@ -6,7 +6,7 @@ export default function CourseTopics(){
     return (
         <>
         <div id="course-topics" className="mx-2 flex min-w-0 w-full flex-1 flex-col items-start justify-start gap-[1vh] pt-5">
-            <h4 className="font-semibold tracking-widest text-md">Topics for This Course</h4>
+            <h5 className="font-semibold tracking-widest text-md">Topics for This Course</h5>
             <ProgressBar value={35} />
             <div className="flex w-full flex-col gap-4">
               {courseTopicsData.map((section) => (
